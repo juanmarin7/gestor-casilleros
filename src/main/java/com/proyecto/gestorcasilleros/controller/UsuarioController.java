33523c07ecf2ea9932c79cc36ab2fe0b0c78562c
Juan Esteban Cano Marin <juanestebanc6@gmail.com>
2025-03-08 11:11:41 -0500

    se agrega la dependencia swagger para la documentacion de la API

--- src/main/java/com/proyecto/gestorcasilleros/controller/UsuarioController.java
+++ src/main/java/com/proyecto/gestorcasilleros/controller/UsuarioController.java
@@ -6,11 +6,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 //se usa para que el navegador permita recibir las peticiones entrantes
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/usuarios")
+@Tag(name = "Usuarios", description = "Operaciones relacionadas con los usuarios") // Categoría en Swagger
 public class UsuarioController {
     private final UsuarioService usuarioService;
 
@@ -19,11 +22,13 @@ public class UsuarioController {
     }
 
     //se usa para probar la conexion inicial a la bd al desplegar el proyecto
+    @Operation(summary = "Obtener todos los usuarios", description = "Devuelve una lista con todos los usuarios registrados")
     @GetMapping
     public ResponseEntity<List<Usuario>> obtenerTodos() {
         return ResponseEntity.ok(usuarioService.obtenerTodos());
     }
     //se realiza el registro o se devuelvo un error al registrar
+    @Operation(summary = "Registrar un usuario", description = "Permite registrar un nuevo usuario en la base de datos")
     @PostMapping("/registrar")
     public ResponseEntity<String> registrarUsuario(@RequestBody Usuario usuario) {
         return usuarioService.guardar(usuario) ?
@@ -31,6 +36,7 @@ public class UsuarioController {
                 ResponseEntity.badRequest().body("Error al registrar usuario");
     }
     //se comparan los datos recibidos con los de la bd para mostrar un mensaje
+    @Operation(summary = "Login de usuario", description = "Verifica las credenciales y permite el inicio de sesión")
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody Usuario usuario) {
         System.out.println("logeado");
@@ -41,6 +47,7 @@ public class UsuarioController {
                 ResponseEntity.status(401).body("Credenciales incorrectas");
     }
     //en el momento no se esta usando pero servira para actualizar datos de un usuario
+    @Operation(summary = "Actualizar usuario", description = "Permite actualizar la información de un usuario existente")
     @PutMapping
     public ResponseEntity<String> actualizar(@RequestBody Usuario usuario) {
         return usuarioService.actualizar(usuario) ?
@@ -48,6 +55,7 @@ public class UsuarioController {
                 ResponseEntity.badRequest().body("Error al actualizar usuario");
     }
     //prueba de conexion a la bd
+    @Operation(summary = "Probar conexión con la base de datos", description = "Verifica si la API tiene conexión con la BD")
     @GetMapping("/testConexion")
     public ResponseEntity<String> probarConexion() {
         try {
